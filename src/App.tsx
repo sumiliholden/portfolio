0a7cc656bcf2ab6bf2237a28a6a5e28a85236582
Holden Jay Sumili <holden@codesquirrel.com.au>
2026-04-01 05:19:01 +0800

    fix: build errors

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Experience from './components/Experience.tsx';
 import PortfolioProjects from './components/Portfolio.tsx';
 import Contact from './components/Contact.tsx';
 import Footer from './components/Footer.tsx';
-import Test from './components/Test.tsx';
 function App() {
   return (
     <div className="w-full">
